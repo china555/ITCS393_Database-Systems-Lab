d1cc04740d9103d65ad3737961a7e4525e432203
Chinapanda <kittikornchina2014@gmail.com>
2021-02-23 10:39:29 +0700

    finish

--- week 6/Lab06.sql
+++ week 6/Lab06.sql
@@ -1,12 +1,17 @@
-use adventureworks
-/*  Student Name___________________________ ID_______*/
+use adventureworks2014
+/*  Student NameKittikorn Keeratikriengkrai ID6188086 */
 
 /*  Q1 Write a query that returns BusinessEntityID, LastName ,
     NationalIDNumber, and JobTitle of all Person.
 	[Table: Person, Employee]
  */
 -- Your Solution is here!
- 
+SELECT p.BusinessEntityID, p.LastName, e.NationalIDNumber, e.JobTitle
+FROM Person.Person p
+	JOIN HumanResources.Employee e on e.BusinessEntityID = p.BusinessEntityID
+
+
+
 
 
 /*	Q2 Write a query that returns FirstName, LastName and 
@@ -14,6 +19,9 @@ use adventureworks
 	[Table: Person, PersonCreditCard]	
 */
 -- Your Solution is here!
+SELECT p.FirstName, p.LastName, pc.CreditCardID
+FROM Person.Person p
+	RIGHT JOIN Sales.PersonCreditCard pc on pc.BusinessEntityID = p.BusinessEntityID
 
 
 
@@ -22,13 +30,23 @@ use adventureworks
 	of Employee who were assigned the department and  loginID.
      [Table: Person, Employee, Department, EmployeeDepartmentHistory]
 	*/
--- Your Solution is here! 
+-- Your Solution is here!
+SELECT p.FirstName, LoginID, hd.Name
+FROM Person.Person p
+	JOIN HumanResources.Employee he on he.BusinessEntityID = p.BusinessEntityID
+	JOIN HumanResources.EmployeeDepartmentHistory eh on eh.BusinessEntityID = he.BusinessEntityID
+	JOIN HumanResources.Department hd on hd.DepartmentID = eh.DepartmentID
+
 
 
 
 /*	Q4 Write a query that returns one column called Result and 
 	contains the last name of the employee with NationalIDNumber 112457891.
     [Table: Person, Employee]
-*/ 
+*/
 -- Your Solution is here!
+SELECT CONCAT(LastName,NationalIDNumber )AS Result
+FROM Person.Person p
+	JOIN HumanResources.Employee he on he.BusinessEntityID = p.BusinessEntityID
+Where NationalIDNumber = 112457891
  
